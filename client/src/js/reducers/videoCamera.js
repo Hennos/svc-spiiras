d6ea10693e244b0e9279f3f9a71e50eee5a842d7
Evgeny Karasev <gnomskg@gmail.com>
2016-08-23 14:45:33 +0300

    Ended rewritting reducers to more confortable state  Changes to be committed: 	modified:   client/src/js/reducers/videoCamera.js

--- client/src/js/reducers/videoCamera.js
+++ client/src/js/reducers/videoCamera.js
@@ -5,17 +5,25 @@ import {state as initialState} from '../states/videoCamera'
 const videoCameraComponent = (state = initialState, action) => {
   switch (action.type) {
     case VIDEO_CAMERA_LOADING:
-      console.log(VIDEO_CAMERA_LOADING);
-      return state.set(Parameters.isLoading, !state.get(Parameters.isLoading));
+      return handleVideoCameraLoading(state, action);
     case TOGGLE_VIDEO_CAMERA_STATE:
-      console.log(TOGGLE_VIDEO_CAMERA_STATE);
-      return state.withMutations((ctx) => {
-        ctx.set(Parameters.isLoading, false)
-          .set(Parameters.isWorking, !state.get(Parameters.isWorking))
-      });
+      return handleTogglingCameraState(state, action);
     default:
       return state;
   }
 };
+
+function handleVideoCameraLoading(state, action) {
+  console.log(VIDEO_CAMERA_LOADING);
+  return state.set(Parameters.isLoading, !state.get(Parameters.isLoading));
+}
+
+function handleTogglingCameraState(state, action) {
+  console.log(TOGGLE_VIDEO_CAMERA_STATE);
+  return state.withMutations((ctx) => {
+    ctx.set(Parameters.isLoading, false)
+      .set(Parameters.isWorking, !state.get(Parameters.isWorking))
+  });
+}
 
 export default videoCameraComponent;
