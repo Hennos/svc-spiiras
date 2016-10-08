723c49540cc700f73e1bef916969366d3b261481
Evgeny Karasev <gnomskg@gmail.com>
2016-10-09 01:39:28 +0300

    mongoose.Promise = global.Promise  Changes to be committed: 	modified:   server/mongoose/db.js

--- server/mongoose/db.js
+++ server/mongoose/db.js
@@ -3,6 +3,8 @@ var mongoose = require('mongoose');
 
 var options = nconf.get('database');
 
+mongoose.Promise = global.Promise;
+
 mongoose.connect(options.host + '/' + options.name);
 var mongooseConnection = mongoose.connection;
 var User = require('./models/user');
@@ -15,26 +17,6 @@ mongooseConnection.on('error', function (err) {
 
 mongooseConnection.on('open', function (cb) {
   console.info('Database connection established.');
-
-  /*    User.find({}, 'username', function (err, docs) {
-   var _ids = _.pluck(docs, '_id');
-   var names = _.pluck(docs, 'username');
-
-   console.log(names);
-
-   _.each(names, function (name) {
-
-   var allNames = _.without(names, name);
-
-   User.update({username:name}, {$set:{friends:_ids}}, function(err){
-   if(err)
-   console.error(err);
-   });
-
-   });
-
-   });*/
-
 });
 
 module.exports = mongooseConnection;
