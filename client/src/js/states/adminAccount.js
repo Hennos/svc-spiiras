3a69dfdf7670980f859edd09ba21d2ff46b73058
Evgeny Karasev <gnomskg@gmail.com>
2016-11-27 15:00:32 +0300

    Changed default admin states after changing constants

--- client/src/js/states/adminAccount.js
+++ client/src/js/states/adminAccount.js
@@ -1,16 +1,6 @@
 import Immutable from 'immutable';
-import {result as result} from '../constants/adminAccount'
-import {changeuserfromadmin as changeuserfromadmin} from '../constants/adminAccount'
+import {adminAccount} from '../constants/adminAccount'
+
 export const state = Immutable.Map([
-
-  [changeuserfromadmin.username, null],
-  [changeuserfromadmin.password, null],
-  [changeuserfromadmin.makecalls, false],
-  [changeuserfromadmin.addingfriends, false],
-  [changeuserfromadmin.forcedchallenge,false],
-  [changeuserfromadmin.interactiveboard,false],
-  [changeuserfromadmin.passwordexitprofile,false],
-  [changeuserfromadmin.passwordmanipulationofaudiovideo,false],
-  [result, '']
+  [adminAccount.admined, Immutable.Map()]
 ]);
-
